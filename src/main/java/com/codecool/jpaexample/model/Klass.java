23d5327715e9fd662d1a0b83ce3d15ca95591cbd
Orsolya Lévai <solya0082@gmail.com>
2018-05-22 19:09:09 +0200

    Task 8: OneToMany at class

--- src/main/java/com/codecool/jpaexample/model/Klass.java
+++ src/main/java/com/codecool/jpaexample/model/Klass.java
@@ -4,8 +4,18 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "Class")
 public class Klass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     private String name;
+
+    @OneToMany(cascade=CascadeType.ALL)
+    //@JoinColumn(name="student_id")
     private Set<Student> students = new HashSet<>();
 
     public Klass() {}
@@ -22,12 +32,17 @@ public class Klass {
         this.name = name;
     }
 
+
     public Set<Student> getStudents() {
         return students;
     }
 
     public void addStudent(Student student) {
         students.add(student);
+    }
+
+    public long getId() {
+        return id;
     }
 
 }
